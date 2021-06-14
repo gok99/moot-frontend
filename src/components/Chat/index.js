58e32ac0eec0aab0a67b2e19ca5741bd42d3fa7a
LeeHyungWoon <leehyungwoonsamuel@gmail.com>
2021-06-15 00:15:35 +0800

    Home Page Layout added

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { AccountPreview } from '../Account';
+import { PostCreation } from '../Post';
+import { withAuthorization } from '../Session';
+
+import * as ROUTES from '../../constants/routes';
+
+import '../Styles/styles.css';
 
 const Chat = () => (
-  <div>
-    <h1><br /><br />Chat</h1>
-  </div>
+  <Container className="homepage">
+    <Row className="divider"></Row>
+    <Row>
+      <Col>{/* Blank divider */}</Col>
+      <Col xs={8}>
+        <Row>
+          <Col xs={4}>
+            <div className="fixed">
+              <AccountPreview />
+              <br />
+              <PostCreation />
+            </div>
+          </Col>
+          <Col xs={8}>
+            <h1>Chats...</h1>
+          </Col>
+        </Row>
+      </Col>
+      <Col>{/* Blank divider */}</Col>
+    </Row>
+  </ Container>
 );
 
-export default Chat;
+const dest = authUser => { return {
+  authorized: !!authUser,
+  destination: ROUTES.SIGN_IN,
+};
+}
+
+export default withAuthorization(dest)(Chat);
--- src/components/Friends/index.js
+++ src/components/Friends/index.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { AccountPreview } from '../Account';
+import { PostCreation } from '../Post';
+import { withAuthorization } from '../Session';
+
+import * as ROUTES from '../../constants/routes';
+
+import '../Styles/styles.css';
 
 const Friends = () => (
-  <div>
-    <h1><br /><br />Friends</h1>
-  </div>
+  <Container className="homepage">
+    <Row className="divider"></Row>
+    <Row>
+      <Col>{/* Blank divider */}</Col>
+      <Col xs={8}>
+        <Row>
+          <Col xs={4}>
+            <div className="fixed">
+              <AccountPreview />
+              <br />
+              <PostCreation />
+            </div>
+          </Col>
+          <Col xs={8}>
+            <h1>Friends...</h1>
+          </Col>
+        </Row>
+      </Col>
+      <Col>{/* Blank divider */}</Col>
+    </Row>
+  </ Container>
 );
 
-export default Friends;
+const dest = authUser => { return {
+  authorized: !!authUser,
+  destination: ROUTES.SIGN_IN,
+};
+}
+
+export default withAuthorization(dest)(Friends);
