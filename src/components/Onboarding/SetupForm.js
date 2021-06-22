4afd538e2d5f12f147b7a83f236694d725ade99f
leehyungwoon <leehyungwoonsamuel@gmail.com>
2021-06-22 23:27:50 +0800

    fix account page verification flow

--- src/components/Onboarding/SetupForm.js
+++ src/components/Onboarding/SetupForm.js
@@ -13,12 +13,14 @@ import '../Styles/styles.css';
 class SetupFormBase extends Component {
   constructor(props) {
     super(props);
-    this.state = { data: { teleUser: '' } };
+    this.state = { visibleID: false, data: { teleUser: '', id: 0 } };
   }
 
   componentDidMount() {
     const fb = this.props.firebase;
     const uid = fb.auth.currentUser.uid;
+    const emailVerified = fb.auth.currentUser.emailVerified;
+    this.setState({ 'visibleID': emailVerified }); 
     const user = fb.user(uid).once('value').then((snapshot) => {
           if (snapshot.exists()) {
               return snapshot.val();
@@ -40,22 +42,24 @@ class SetupFormBase extends Component {
           <div className="contentbox spacedbox">
             <h3>ACCOUNT VERIFICATION</h3>
             <hr />
-            <p className="accounttext">Clicking the button below will re-send the verification email to your NUSNET Email. Please use this if you need to re-verify your account.</p><br />
+            <p className="accounttext">Clicking the button below will re-send the verification email to your NUSNET Email. Please use this if you did not receive / cannot find the verification email.</p><br />
             <ResendEmailForm />
           </div>
           <div className="contentbox spacedbox">
             <h3>TELEGRAM CONNECTION</h3>
             <hr />
-            { this.state.data.teleUser === ''
+            { this.state.visibleID && this.state.data.teleUser !== ''
               ? <div>
-                  <p className="accounttext">To connect to telegram, please click the button below to access the main telegram bot, and input the command "/r [User ID]".</p>
-                  <p className="accounttext">(E.g. "/r 000000000")</p><br />
-                  <p className="accounttext">The User ID is the 9-digit numerical code that should be visible above if you have already verified your email. If you don't see the code, please proceed to verify your email: you can have the verification email re-sent to your email via above.</p><br />
-                  <TelegramForm />
-                </div>
-              : <div>
                   <p className="accounttext">Hello, @{ this.state.data.teleUser }! You are currently connected to telegram.</p>
                 </div>
+              : this.state.visibleID
+                ? <div>
+                    <p className="accounttext">To connect to telegram, please click the button below to access the main telegram bot, and input the command "/r { this.state.data.id }".</p><br />
+                    <TelegramForm />
+                  </div>
+                : <div>
+                    <p className="accounttext">Please verify your email first so that you can access this feature. Thank you!</p>
+                  </div>
             }
           </div>
           </div>
