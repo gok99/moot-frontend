89ae23dd7fdf46b3b38075c401c22987909f7671
LeeHyungWoon <leehyungwoonsamuel@gmail.com>
2021-07-23 16:16:34 +0800

    Frenny

--- src/components/Account/ProfilePicture/index.js
+++ src/components/Account/ProfilePicture/index.js
@@ -1,28 +1,23 @@
 import React from 'react';
  
 import '../../Styles/styles.css';
-import icz1 from '../../../assets/icon_z/icon_z0.png';
-import icz2 from '../../../assets/icon_z/icon_z0.png';
-import icz3 from '../../../assets/icon_z/icon_z0.png';
-import icz4 from '../../../assets/icon_z/icon_z0.png';
-import icz5 from '../../../assets/icon_z/icon_z0.png';
-import icz6 from '../../../assets/icon_z/icon_z0.png';
-import icz7 from '../../../assets/icon_z/icon_z0.png';
-import icz8 from '../../../assets/icon_z/icon_z0.png';
-import icz9 from '../../../assets/icon_z/icon_z0.png';
-import icz10 from '../../../assets/icon_z/icon_z0.png';
-import icz11 from '../../../assets/icon_z/icon_z0.png';
-import icz12 from '../../../assets/icon_z/icon_z0.png';
+import icfrtl from '../../../assets/icon_frtl.png';
+import icfrtr from '../../../assets/icon_frtr.png';
+import icfrf from '../../../assets/icon_frf.png';
+import icfrbl from '../../../assets/icon_frbl.png';
+import icfrbr from '../../../assets/icon_frbr.png';
 
 /**
  * Functional Container Component that retrieves and returns the correct profile picture based on user pid.
  */
 const ProfilePicture = (props) => {
-  const pid = props.pid;
+  // const pid = props.pid;
+  const account = props.account;
   const style = props.picStyle;
-  const iczArray = [icz1, icz2, icz3, icz4, icz5, icz6, icz7, icz8, icz9, icz10, icz11, icz12];
+  const currPid = account ? 2 : Math.floor(Math.random() * 6);
+  const icArray = [icfrtl, icfrtr, icfrf, icfrbl, icfrbr];
   return (
-    <img className={"img-profile " + style} src={iczArray[pid-1]} alt=" " />
+    <img className={"img-profile " + style} src={icArray[currPid]} alt=" " />
   );
 };
 
